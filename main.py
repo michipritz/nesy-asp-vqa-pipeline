0a9561d6aa9407713cb305cf50e32aac5d005eb4
MPritz <michael-pritz@gmx.at>
2021-04-18 19:09:53 +0200

    Added wrong question statistics to output

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ import time
 from translate import translate
 
 
-def print_stats(total, correct, invalid):
+def print_stats(total, correct, wrong, invalid):
     correct_rel = correct / total
+    wrong_rel = wrong / total
     invalid_rel = invalid / total
 
     print("Questions total: " + str(total))
-    print("Questions correct: " + str(correct) + "\t\t(" + str(correct_rel) + ")")
-    print("Questions invalid: " + str(invalid) + "\t\t(" + str(invalid_rel) + ")\n")
+    print("Questions correct: " + str(correct) + " (" + str(correct_rel) + ")")
+    print("Questions wrong: " + str(wrong) + " (" + str(wrong_rel) + ")")
+    print("Questions invalid: " + str(invalid) + " (" + str(invalid_rel) + ")\n")
 
 
 if __name__ == "__main__":
@@ -34,6 +36,7 @@ if __name__ == "__main__":
 
     q_total = 0
     q_correct = 0
+    q_wrong = 0
     q_invalid = 0
 
     start_time = time.time()
@@ -75,7 +78,7 @@ if __name__ == "__main__":
 
         for j, q in enumerate(questions_dict[i]):
             if q_total % 5000 == 0 and q_total > 0:
-                print_stats(q_total, q_correct, q_invalid)
+                print_stats(q_total, q_correct, q_wrong, q_invalid)
 
             q_asp = translate(q["program"])
             composite_program = program + q_asp
@@ -99,15 +102,23 @@ if __name__ == "__main__":
                 if guess_val.isnumeric():
                     if guess_val == str(ground_truth):
                         q_correct += 1
+                    else:
+                        q_wrong += 1
                 elif guess_val == "true":
                     if ground_truth == "yes":
                         q_correct += 1
+                    else:
+                        q_wrong += 1
                 elif guess_val == "false":
                     if ground_truth == "no":
                         q_correct += 1
+                    else:
+                        q_wrong += 1
                 else:
                     if ground_truth == guess_val:
                         q_correct += 1
+                    else:
+                        q_wrong += 1
             else:
                 q_invalid += 1
 
@@ -116,4 +127,4 @@ if __name__ == "__main__":
     end_time = time.time()
 
     print("Total time (s): " + str((end_time - start_time)))
-    print_stats(q_total, q_correct, q_invalid)
+    print_stats(q_total, q_correct, q_wrong, q_invalid)
